ae350aafc5472689b3081bfcd5f72d86ceb5827f
qinqiang1990 <1247686880@qq.com>
2015-07-13 08:00:19 +0800

    shiro

---
+++ src/test/java/chapter5/realm/MyRealm2.java
@@ -0,0 +1,31 @@
+package chapter5.realm;
+
+ 
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.credential.PasswordService;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+ 
+public class MyRealm2 extends AuthorizingRealm {
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = "liu"; //用户名及salt1
+        String salt2 = "fad4f4e402ede5f5ef19bb907ea12778";
+        String password = "f071d4b8f2eb93e58f26f175809c30a0"; //加密后的密码
+        SimpleAuthenticationInfo ai = new SimpleAuthenticationInfo(username, password, getName());
+        ai.setCredentialsSalt(ByteSource.Util.bytes(username+salt2)); //盐是用户名+随机数
+        return ai;
+    }
+}
